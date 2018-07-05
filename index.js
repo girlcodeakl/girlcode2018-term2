1079fa740b0610a3f0fd8359be6595cad5fad57f
Tanya <johnsont13@gmail.com>
2018-07-05 19:15:30 +1200

    Added Author
    Added a new field for author name that posts to the feed

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ app.get('/posts', sendPostsList);
 
 //let a client POST something new
 function saveNewPost(request, response) {
-  console.log(request.body.message); //write it on the command prompt so we can see
+  console.log(request.body.message);
+  console.log(request.body.author); //write it on the command prompt so we can see
   let post= {};
+  post.author = request.body.author;
   post.message = request.body.message;
   post.time = new Date();
   posts.push(post);
